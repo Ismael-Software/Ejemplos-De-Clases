907792441ef62856317601b03633742a86dead46
José María <sa546357@uaeh.edu.mx>
2025-10-06 23:38:46 -0600

    Ahora este es la nueva versión de Persona

--- Persona.java
+++ Persona.java
@@ -1,49 +1,77 @@
+import java.util.Scanner;
+
 public class Persona {    
     
+    String trabajo;
     String nombre;
     String apellido;
     int edad;
     
-    Persona(String nom, String ape, int e) {
-        this.nombre = nom;
-        this.apellido = ape;
-        this.edad = e;
+    Persona (String trabajo, String nombre, String apellido, int edad) {
+        this.trabajo = trabajo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
     }
     
     void saludar() {
-        System.out.println("Hola, amigos");
-    }
-    
-    void comer() {
-        System.out.println("Tengo hambre");
+        System.out.println("Hola amigos, este esta persona es un: ");
     }
     
     void mostrarInfo() {
-        System.out.println("Nombre: " + this.nombre + "\nApellido: " + this.apellido + "\nEdad: " + this.edad);
+        System.out.println(this.trabajo + "\nSu nombre es: " + this.nombre + "\nSu apellido es: " + this.apellido + "\nY su edad es: " + this.edad);
+    }
+    
+    void comer() {
+        System.out.println("Y quiere comer una sandía");
     }
     
     public static void main(String[] args) {
-        Persona profesor = new Persona("Alejandro", "Juárez", 47);
-        Persona alumno = new Persona("Daniela", "Resendiz", 17);
-        Persona vendedor = new Persona("Óscar", "Ramírez", 58);
+       
+        String auxTrabajo;
+        String auxNombre;
+        String auxApellido;
+        int auxEdad;
         
-        profesor.mostrarInfo();
+        Scanner entra = new Scanner(System.in);   //Estancia - Inicio de instanciación
+        System.out.println("**********PROFESOR**********");
+        System.out.print("Trabajo: "); auxTrabajo=entra.nextLine();
+        System.out.print("Nombre: "); auxNombre=entra.nextLine();
+        System.out.print("Apellido: "); auxApellido=entra.nextLine();
+        System.out.println("Edad: "); auxEdad=entra.nextInt();
+    
+        System.out.println("**********ALUMNO**********");
+        System.out.print("Trabajo: "); auxTrabajo=entra.nextLine();
+        System.out.print("Nombre: "); auxNombre=entra.nextLine();
+        System.out.print("Apellido: "); auxApellido=entra.nextLine();
+        System.out.println("Edad: "); auxEdad=entra.nextInt();
+        
+        System.out.println("**********VENDEDOR**********");
+        System.out.print("Trabajo: "); auxTrabajo=entra.nextLine();
+        System.out.print("Nombre: "); auxNombre=entra.nextLine();
+        System.out.print("Apellido: "); auxApellido=entra.nextLine();
+        System.out.println("Edad: "); auxEdad=entra.nextInt();
+        
+        Persona profesor = new Persona(auxTrabajo, auxNombre, auxApellido, auxEdad);
+        Persona alumno = new Persona(auxTrabajo, auxNombre, auxApellido, auxEdad);
+        Persona vendedor = new Persona(auxTrabajo, auxNombre, auxApellido, auxEdad);
+       
         profesor.saludar();
+        profesor.mostrarInfo();
         profesor.comer();
         System.out.println();
-
-        alumno.mostrarInfo();
+       
         alumno.saludar();
+        alumno.mostrarInfo();
         alumno.comer();
         System.out.println();
-
-        vendedor.mostrarInfo();
-        vendedor.saludar();
-        vendedor.comer();
         
+        vendedor.saludar();
+        vendedor.mostrarInfo();
+        vendedor.comer();
+        System.out.println();
+        
+        
+       
     }
-       
-       
-     
- 
 }
